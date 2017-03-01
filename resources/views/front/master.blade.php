ecc325fe56a258e8dd2a14e12833062132b9ef7a
jiangzhou9 <976912502@qq.com>
2017-03-01 15:11:54 +0800

    master

--- resources/views/front/master.blade.php
+++ resources/views/front/master.blade.php
@@ -5,6 +5,30 @@
     <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
 </head>
 <body>
+{{--header begin--}}
+<div class="index-header">
+    <div class="header-content">
+        <div class="header-log">
+            <img src="#">
+        </div>
+        <div class="header-menu">
+            <ul>
+                <li><a href="#">首页</a></li>
+                <li><a href="#">介绍</a></li>
+                <li><a href="#">动态</a></li>
+                <li><a href="#">联系我们</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+{{--header end--}}
 
+{{--footer begin--}}
+<div class="index-footer">
+    <div class="footer-content">
+        <div></div>
+    </div>
+</div>
+{{--footer end--}}
 </body>
 </html>
