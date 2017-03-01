666cf40277e5129cb82cb33a7cdb8910dc540927
jiangzhou9 <976912502@qq.com>
2017-03-01 17:37:01 +0800

    master

--- resources/views/front/master.blade.php
+++ resources/views/front/master.blade.php
@@ -3,6 +3,7 @@
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <title>华创益网</title>
     <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
 </head>
 <body>
 {{--header begin--}}
@@ -21,9 +22,7 @@
         </div>
         <div class="header-other header-item">
             <ul>
-                <li>联系方式：</li>
-                <li>邮箱：123123123@qq.com</li>
-                <li>手机：12312312312312312</li>
+                <li>联系邮箱：123123@qq.com</li>
             </ul>
         </div>
     </div>
@@ -33,11 +32,21 @@
 {{--footer begin--}}
 <div class="index-footer">
     <div class="footer-wrapper">
-        <div></div>
+        <div class="footer-item"><i class="fa fa-check-square-o fa-4x"></i><h1>高效</h1></div>
+        <div class="footer-item"><i class="fa fa-check-square-o fa-4x"></i><h1>便捷</h1></div>
+        <div class="footer-item"><i class="fa fa-check-square-o fa-4x"></i><h1>安全</h1></div>
+        <div class="footer-item"><i class="fa fa-check-square-o fa-4x"></i><h1>美观</h1></div>
     </div>
 </div>
 <div class="index-web-info">
-
+    <div class="web-info-wrapper">
+        <div class="web-info-left web-info-item">
+            <span>Copyright © 1999-2017 青岛华创益网信息技术有限公司 All Rights Reserved</span>
+        </div>
+        <div class="web-info-right web-info-item">
+            <a href="http://www.miitbeian.gov.cn/">鲁ICP备16009379号-1</a>
+        </div>
+    </div>
 </div>
 {{--footer end--}}
 </body>
