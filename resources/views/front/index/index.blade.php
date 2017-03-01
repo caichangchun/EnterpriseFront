5f56d139701b90c14d1760c87265a036bb11a743
wangqinglu <1119543546@qq.com>
2017-03-01 18:23:45 +0800

    edit

--- resources/views/front/index/index.blade.php
+++ resources/views/front/index/index.blade.php
@@ -6,24 +6,32 @@
     <style>
         .banner-list
         {
+            position: relative;
             width: 100%;
+            overflow: hidden;
+            height: 500px;
         }
         .banner-img
         {
+            width: 1920px;
             position: absolute;
-            width: 100%;
+            left: 50%;
+            margin-left: -971px;
+            height: 500px;
+        }
+        #banner_switch
+        {
+            margin-top: 500px;
         }
     </style>
     <div id="banner_list" class="banner-list">
         <img class="banner-img" src="/images/1.jpg">
         <img class="banner-img" src="/images/2.jpg">
-        <img class="banner-img" src="/images/3.jpg">
-        <img class="banner-img" src="/images/4.jpg">
+
     </div>
-    <br><br><br><br><br><br><br><br><br>
     <div id="banner_switch">
         <ul style="list-style-type: none; z-index: 2">
-            <li style="float: left; margin-left: 10px cursor: pointer">1</li>;
+            <li style="float: left; margin-left: 10px; cursor: pointer">1</li>;
             <li style="float: left; margin-left: 10px; cursor: pointer">2</li>
             <li style="float: left; margin-left: 10px; cursor: pointer">3</li>
             <li style="float: left; margin-left: 10px; cursor: pointer">4</li>
@@ -33,7 +41,6 @@
 
 @section('script')
     <script>
-
         $(document).ready(function () {
             var current_img = 0;
             $('.banner-img').each(function () {
